34e393f47529d7141c051b63e228bf407c415b02
Chris Hill <26716179+ChrisHillCap@users.noreply.github.com>
2019-10-24 09:02:42 +0100

    Update jira-updated-status-by-filter-tampermonkey.user.js

--- jira-updated-status-by-filter-tampermonkey.user.js
+++ jira-updated-status-by-filter-tampermonkey.user.js
@@ -9,158 +9,209 @@
 // ==/UserScript==
 
 (function() {
-     function addSubmitForm() {
-         var divElement = document.createElement("div")
 
-         var labelForFilterId = document.createElement("label");
-         labelForFilterId.innerHTML = "Please enter a valid filter id for issues you wish to see the 'when set to in progress' timestamp'"
-         labelForFilterId.setAttribute("for","filter-id-tamper-monkey");
+    const listOfStatusesWeCareAbout = ["Dev Ready", "started", "In Development","Ready for Review","Done"]
+    'use strict';
+    const windowLocationJira = window.location.origin
 
-          var labelForAmountOfIssuesToSearch = document.createElement("label");
-         labelForAmountOfIssuesToSearch.innerHTML = "Please enter the number of issues in the filter you wish to search in, Please be aware of how big your filter is. EVERY ISSUE makes a request to the Jira API. 4000 items in a filter is 4000 requests. "
-         labelForAmountOfIssuesToSearch.setAttribute("for","amount-of-records-tamper-monkey");
-        const buttonForSubmitFilter = document.createElement("button");
-         buttonForSubmitFilter.innerHTML = "Return stats";
+    function addBr(element) {
+        return element.innerHTML += "<br>"
+    }
+
+    function addElementToMainPage(element) {
+        const pageElem = document.getElementById("page")
+        if(pageElem == null) {
+            document.getElementsByTagName("body")[0].appendChild(element)
+            return "body";
+        } else {
+            pageElem.appendChild(element);
+            return "page";
+        }
+    }
+
+    function addSubmitForm() {
+
+        var divElement = document.createElement("div")
+
+        var labelForFilterId = document.createElement("label");
+        labelForFilterId.innerHTML = "Please enter a valid filter id for issues you wish to see the 'when set to in progress' timestamp'"
+        labelForFilterId.setAttribute("for","filter-id-tamper-monkey");
+
+        var labelForAmountOfIssuesToSearch = document.createElement("label");
+        labelForAmountOfIssuesToSearch.innerHTML = "Please enter the number of issues in the filter you wish to search in, Please be aware of how big your filter is. EVERY ISSUE makes a request to the Jira API. 4000 items in a filter is 4000 requests. "
+        labelForAmountOfIssuesToSearch.setAttribute("for","amount-of-records-tamper-monkey");
+
+        var buttonForSubmitFilter = document.createElement("button");
+        buttonForSubmitFilter.innerHTML = "Return stats";
         buttonForSubmitFilter.id = "return-stats-tamper-monkey";
-        const textBoxForFilterId = document.createElement("input");
+
+        var textBoxForFilterId = document.createElement("input");
         textBoxForFilterId.type = "text";
         textBoxForFilterId.id = "filter-id-tamper-monkey";
-        const textBoxAmountOfRecordsToSearchForInFilter = document.createElement("input");
+
+        var textBoxAmountOfRecordsToSearchForInFilter = document.createElement("input");
         textBoxAmountOfRecordsToSearchForInFilter.type = "text";
         textBoxAmountOfRecordsToSearchForInFilter.id = "amount-of-records-tamper-monkey";
+        textBoxAmountOfRecordsToSearchForInFilter.defaultValue = "150";
 
-       buttonForSubmitFilter.addEventListener ("click", validateBeforeCallingApis , false);
-         divElement.innerHTML += "<br>"
-         divElement.appendChild(labelForFilterId);
-         divElement.innerHTML += "<br>"
-         divElement.appendChild(textBoxForFilterId);
-         divElement.innerHTML += "<br>"
-         divElement.appendChild(labelForAmountOfIssuesToSearch);
-         divElement.innerHTML += "<br>"
-         divElement.appendChild(textBoxAmountOfRecordsToSearchForInFilter);
-         divElement.innerHTML += "<br>"
-         divElement.appendChild(buttonForSubmitFilter);
-       document.getElementById("page").appendChild(divElement);
+        buttonForSubmitFilter.addEventListener ("click", validateBeforeCallingApis , false);
+        addBr(divElement)
+        divElement.appendChild(labelForFilterId);
+        addBr(divElement)
+        divElement.appendChild(textBoxForFilterId);
+        addBr(divElement)
+        divElement.appendChild(labelForAmountOfIssuesToSearch);
+        addBr(divElement)
+        divElement.appendChild(textBoxAmountOfRecordsToSearchForInFilter);
+        addBr(divElement)
+        divElement.appendChild(buttonForSubmitFilter);
+
+        addElementToMainPage(divElement);
 
     }
     addSubmitForm();
 
-    const windowLocationJira = window.location.origin
-
-    const urlParamFilterNumber = new URLSearchParams(window.location.search).get('filter');
-
-    //not my func https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_table_desc
+    //not my func: https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_sort_table_desc
     function sortTable() {
-  var table, rows, switching, i, x, y, shouldSwitch;
-  table = document.getElementById("issuesWithInProgress");
-  switching = true;
-  /*Make a loop that will continue until
+        var table, rows, switching, i, x, y, shouldSwitch;
+        table = document.getElementById("issuesWithInProgress");
+        switching = true;
+        /*Make a loop that will continue until
   no switching has been done:*/
-  while (switching) {
-    //start by saying: no switching is done:
-    switching = false;
-    rows = table.rows;
-    /*Loop through all table rows (except the
+        while (switching) {
+            //start by saying: no switching is done:
+            switching = false;
+            rows = table.rows;
+            /*Loop through all table rows (except the
     first, which contains table headers):*/
-    for (i = 1; i < (rows.length - 1); i++) {
-      //start by saying there should be no switching:
-      shouldSwitch = false;
-      /*Get the two elements you want to compare,
+            for (i = 1; i < (rows.length - 1); i++) {
+                //start by saying there should be no switching:
+                shouldSwitch = false;
+                /*Get the two elements you want to compare,
       one from current row and one from the next:*/
-      x = rows[i].getElementsByTagName("TD")[0];
-      y = rows[i + 1].getElementsByTagName("TD")[0];
-      //check if the two rows should switch place:
-      if (Date.parse(x.innerHTML) > Date.parse(y.innerHTML)) {
-        //if so, mark as a switch and break the loop:
-        shouldSwitch = true;
-        break;
-      }
-    }
-    if (shouldSwitch) {
-      /*If a switch has been marked, make the switch
+                x = rows[i].getElementsByTagName("TD")[0];
+                y = rows[i + 1].getElementsByTagName("TD")[0];
+                //check if the two rows should switch place:
+                if (Date.parse(x.innerHTML) > Date.parse(y.innerHTML)) {
+                    //if so, mark as a switch and break the loop:
+                    shouldSwitch = true;
+                    break;
+                }
+            }
+            if (shouldSwitch) {
+                /*If a switch has been marked, make the switch
       and mark that a switch has been done:*/
-      rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
-      switching = true;
-    }
-  }
-}
-    'use strict';
-
-  function callApisWithFilterAndGetResults(filterId, amountOfRecords) {
-
-    var leTable = document.createElement("table");
-    var buttonForSort = document.createElement("button");
-    buttonForSort.addEventListener ("click", sortTable , false);
-    buttonForSort.innerHTML = "Sort Table By Date";
-    //set table attributes
-    leTable.setAttribute("border","1");
-    leTable.setAttribute("id","issuesWithInProgress");
-    var header = leTable.createTHead();
-    var rowForHeader = header.insertRow(0);
-    var updatedTitle = rowForHeader.insertCell(0);
-    var mainTitle = rowForHeader.insertCell(1);
-    updatedTitle.innerHTML = "<b>Updated timestamp of change in ticket</b>";
-    mainTitle.innerHTML = "<b>Title of ticket</b>";
-
-    var tableRowCount = 1;
-
-     async function getAllIssuesReturn() {
-
-         return fetch(windowLocationJira + "/rest/api/2/search?jql=filter=" + filterId +"&maxResults=" + amountOfRecords).then(response => response.text()).then(text => {
-        var i;
-               var keys = [];
-        const jsonDocument = JSON.parse(text);
-        console.log(jsonDocument);
-        const issues = jsonDocument.issues;
-           for (i = 0; i < issues.length; ++i) {
-
-               keys.push(issues[i].key);
-           }
-             return keys;
-    });
-
-     }
-    getAllIssuesReturn().then(issueKeys => {
-
-
-    var countFoo;
-    for (countFoo = 0; countFoo < issueKeys.length; countFoo++) {
-
-    fetch(windowLocationJira + "/activity?maxResults=1000&streams=issue-key+IS+" + issueKeys[countFoo] + "&os_authType=basic&title=undefined&orderBy=Status").then(response => response.text()).then(text => {
-
-    var parser = new DOMParser();
-    var xmlDocument = parser.parseFromString(text, "text/xml");
-
-    var activityEntries = xmlDocument.getElementsByTagName("entry");
-
-    var i;
-    for (i = 0; i < activityEntries.length; i++) {
-     
-    if(activityEntries[i].getElementsByTagName("title")[0].childNodes[0].nodeValue.includes("started progress on")) {
-
-      var row = leTable.insertRow(tableRowCount);
-
-      var updatedTimestamp = row.insertCell(0);
-      var title = row.insertCell(1);
-
-      updatedTimestamp.innerHTML = activityEntries[i].getElementsByTagName("updated")[0].childNodes[0].nodeValue
-      title.innerHTML = activityEntries[i].getElementsByTagName("title")[0].childNodes[0].nodeValue
-      tableRowCount++
-  }
-}
-     document.getElementsByTagName("body")[0].appendChild(buttonForSort);
-     document.getElementsByTagName("body")[0].appendChild(leTable);
-
-   })
+                rows[i].parentNode.insertBefore(rows[i + 1], rows[i]);
+                switching = true;
+            }
         }
-    });
-  }
+    }
+    async function getAllIssuesReturn(filterId, amountOfRecords) {
+
+        return fetch(windowLocationJira + "/rest/api/2/search?jql=filter=" + filterId +"&maxResults=" + amountOfRecords).then(response => response.text()).then(text => {
+
+            const jsonDocument = JSON.parse(text);
+            const issues = jsonDocument.issues;
+
+            var keys = [];
+            var i;
+            for (i = 0; i < issues.length; ++i) {
+                keys.push(issues[i].key);
+            }
+            return keys;
+        });
+
+    }
+
+
+    function callApisWithFilterAndGetResults(filterId, amountOfRecords) {
+
+        var leTable = document.createElement("table");
+        var buttonForSort = document.createElement("button");
+        buttonForSort.addEventListener ("click", sortTable , false);
+        buttonForSort.innerHTML = "Sort Table By Date";
+        //set table attributes
+        leTable.setAttribute("border","1");
+        leTable.setAttribute("id","issuesWithInProgress");
+        var header = leTable.createTHead();
+        var rowForHeader = header.insertRow(0);
+        var updatedTitle = rowForHeader.insertCell(0);
+        var mainTitle = rowForHeader.insertCell(1);
+        var statusChangeTo = rowForHeader.insertCell(2);
+        var issueIdHeader = rowForHeader.insertCell(3);
+        updatedTitle.innerHTML = "<b>Updated timestamp of change in ticket</b>";
+        mainTitle.innerHTML = "<b>Title of ticket</b>";
+        statusChangeTo.innerHTML = "<b>Changed to Status</b>";
+        issueIdHeader.innerHTML = "<b> Issue Id </b>";
+
+        var tableRowCount = 1;
+
+        getAllIssuesReturn(filterId, amountOfRecords).then(issueKeys => {
+
+            function extractTheCategory(entry) {
+                return entry.getElementsByTagName("category")[0]
+            }
+            function getTermAttribute(entry) {
+                return entry.getAttribute("term");
+            }
+            function extractTheUpdatedTimestamp(entry) {
+                return entry.getElementsByTagName("updated")[0].childNodes[0].nodeValue;
+            }
+            function extractTheTitle(entry) {
+                return entry.getElementsByTagName("title")[0].childNodes[0].nodeValue;
+            }
+
+            for (var indexOfIssue = 0; indexOfIssue < issueKeys.length; indexOfIssue++) {
+
+                fetch(windowLocationJira + "/activity?maxResults=1000&streams=issue-key+IS+" + issueKeys[indexOfIssue] + "&os_authType=basic&title=undefined&orderBy=Status")
+                    .then(response => [issueKeys[indexOfIssue],response.text()])
+                    .then(text => {
+                    console.log(text[0]);
+                    console.log(text[1]);
+                    var parser = new DOMParser();
+                    var xmlDocument = parser.parseFromString(text[1], "text/xml");
+
+                    var activityEntries = xmlDocument.getElementsByTagName("entry");
+
+console.log(text[0]);
+                    for (var i = 0; i < activityEntries.length; i++) {
+                        function updateTable(status, updatedTimestamp, titleOfEntry, issueId) {
+                            var row = leTable.insertRow(tableRowCount);
+                            var timestampCell = row.insertCell(0);
+                            var titleCell = row.insertCell(1);
+                            var statusChangedToCell = row.insertCell(2);
+                            var issueIDCell = row.insertCell(3);
+                            statusChangedToCell.innerHTML = status;
+                            timestampCell.innerHTML = updatedTimestamp;
+                            titleCell.innerHTML = titleOfEntry;
+                            issueIDCell.innerHTML = "foo";
+                            return leTable;
+                        }
+
+                        const category = extractTheCategory(activityEntries[i]);
+                        const titleOfEntry = extractTheTitle(activityEntries[i]);
+                        const updatedTimestampOfEntry = extractTheUpdatedTimestamp(activityEntries[i]);
+                        if(category != null) {
+                            const termOfCategory = getTermAttribute(category)
+                            if(listOfStatusesWeCareAbout.includes(termOfCategory)) {
+                                updateTable(termOfCategory, updatedTimestampOfEntry, titleOfEntry, text[0]);
+                            }
+                        }
+
+                    }
+                    document.getElementsByTagName("body")[0].appendChild(buttonForSort);
+                    document.getElementsByTagName("body")[0].appendChild(leTable);
+
+                })
+            }
+        });
+    }
 
     function validateBeforeCallingApis() {
-       function validateString(stringToValidate) {
-             if(stringToValidate.includes(" ") || stringToValidate == "") {
-                 return false
-             } else {return true}
+        function validateString(stringToValidate) {
+            if(stringToValidate.includes(" ") || stringToValidate == "") {
+                return false
+            } else {return true}
         }
 
         const filterTextBox = document.getElementById("filter-id-tamper-monkey");
@@ -169,10 +220,10 @@
         const filterTextBoxTrimmed = filterTextBox.value.trim();
 
         if(!validateString(amountOfRecordsTextBoxTrimmed) || !validateString(filterTextBoxTrimmed)) {
-           alert("**Please ensure that: Filter field has no spaces in the ID** Please ensure that: Amount of records has a number in and has no spaces in it");
-           } else {
-             callApisWithFilterAndGetResults(filterTextBoxTrimmed, amountOfRecordsTextBoxTrimmed)
-           }
+            alert("**Please ensure that: Filter field has no spaces in the ID** Please ensure that: Amount of records has a number in and has no spaces in it");
+        } else {
+            callApisWithFilterAndGetResults(filterTextBoxTrimmed, amountOfRecordsTextBoxTrimmed)
+        }
     }
 
 })();
